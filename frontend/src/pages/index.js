201d291d851806cf61fefdb684cc19cb0a2055f8
Jhfnetboy(Survivor) <jhfnetboy@gmail.com>
2022-08-07 16:00:22 +0800

    EventProfile edit

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,5 +1,5 @@
 export {default as Home} from './home/Home';
-export {default as Profile} from './profile/Profile';
+export {default as EventProfile} from './eventProfile/EventProfile';
 export {default as Item} from './item/Item';
 export {default as Create} from './create/Create';
 export {default as CreateEvent} from './createEvent/CreateEvent';
