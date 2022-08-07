d2db8d6c572f3663975739d50cd6217224165e17
Jhfnetboy(Survivor) <jhfnetboy@gmail.com>
2022-08-07 20:44:16 +0800

    error in @gnosis.pm

---
+++ frontend/src/hooks/hooks.js
@@ -0,0 +1,51 @@
+/* Create the IPFS CID of the json data */
+import { NFTStorage } from "nft.storage";
+const useSave2IPFS = async (cName,cDescription,cLogo,cURL) => {
+
+    //image contains any File or Blob you want to save
+    // let imageData = new File(
+    //   [
+    //     `${baseSVG}${name}</text></svg>`,
+    //   ],
+    //   `SoulTokens.svg`,
+    //   {
+    //     type: "image/svg+xml",
+    //   }
+    // );
+    // console.log("imageData:::::",imageData);
+
+    let imageData = cLogo ? cLogo : "";
+
+    let jsonData = {
+      community_name: cName,
+      description: cDescription,
+      attributes: [
+        {"trait_type": "Community Logo",
+        "value": cLogo
+      },       
+        {"trait_type": "Introduction URL",
+      "value": cURL
+      },
+      
+      ],
+      image: imageData,          
+    };
+
+    const client = new NFTStorage({
+      token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDhiNGFGRDdENTBiZDYxOEZlRjhhNDUzMThiYmMwMDk1YjdDMTc5RjEiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY1MTgyNzE0Nzg1OCwibmFtZSI6InRleHR2ZXJzZS1wcmQifQ.nzqaau57VZE-n_RuK5wOV5gVeffDicK8EHrvSKoN7Uo"
+    });
+    try {
+      await client
+        .store(jsonData)
+        .then((metadata) => {
+          // console.log(metadata)
+          console.log("metadata saved", metadata);
+        //   askContractToMintNft(metadata.url);
+        });
+    } catch (error) {
+      console.log(error)
+      console.log("Could not save NFT to NFT.Storage - Aborted minting");
+    }
+  };
+
+  export {useSave2IPFS};
--- frontend/src/pages/create/Create.jsx
+++ frontend/src/pages/create/Create.jsx
@@ -1,18 +1,16 @@
 import './create.css'
+import {useSave2IPFS} from '../../hooks/hooks.js'
 // import Image from '../../assets/Image.png'
 const Create = () => {
-
+  let cName = "Community Name Example";
+  let cDescription ="Community description";
+  let cLogo ="logo";
+  let cURL ="https://123.com"
+  const createCommunity = useSave2IPFS(cName,cDescription,cLogo,cURL);
   return (
     <div className='create section__padding'>
       <div className="create-container">
-        {/* <h1>Create my community</h1> */}
-        {/* <div className="upload-img-show">
-            <h3>JPG, PNG, GIF, SVG, WEBM, MP3, MP4. Max 100mb.</h3>
-            <img src={Image} alt="banner" />
-            <p>Drag and Drop File</p>
-        </div> */}
-        <form className='writeForm' autoComplete='off'>
-          
+        <form className='writeForm' autoComplete='off'>         
           <div className="formGroup">
             <label>Upload a Community logo with 100*100</label>
             <input type="file" className='custom-file-input'
@@ -22,19 +20,13 @@ const Create = () => {
             <label>Set your Community Name limit to 50 words</label>
             <input type="text" placeholder='Community Name' autoFocus={true} />
           </div>
-          {/* <div className="formGroup">
-            <label>Give a Description or just a Motto to introduce</label>
-            <textarea type="text" rows={4}
-          placeholder='Decription of your community' 
-          ></textarea>
-          </div> */}
           <div className="formGroup">
             <label>Give a URL of your Community</label>
             <div className="twoForm">
               <input type="text" placeholder='Community introduction URL'  />
             </div>
           </div>
-          <button className='writeButton'>Create my Community in Registry</button>
+          <button className='writeButton'onClick={createCommunity} >Create my Community in Registry</button>
         </form>
       </div>
     </div>
