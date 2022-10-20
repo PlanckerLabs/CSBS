2103b4b6b2e3f313ea504340dbc748d1166e193d
0xRory <0x1rory@gmail.com>
2022-10-21 07:32:33 +0800

    ImportSBT Pag Send

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,26 +1,27 @@
-import React,{ useState} from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/logo.png'
-import {  Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useAddress, useMetamask, useDisconnect } from "@thirdweb-dev/react";
 
 const Menu = () => (
   <>
-     <Link to="/create"><p>create community </p> </Link>
-     <Link to="/createEvent"><p>create event </p> </Link>
-     <Link to="/eventProfile/Rian"><p>My SBT</p></Link>
-    
-  </>
- )
+    <Link to="/create"><p>create community </p> </Link>
+    <Link to="/createEvent"><p>create event </p> </Link>
+    <Link to="/importSBT"><p>ImportSBT</p> </Link>
+    <Link to="/eventProfile/Rian"><p>My SBT</p></Link>
 
- const Navbar = () => {
+  </>
+)
+
+const Navbar = () => {
   const address = useAddress();
   const connectWithMetamask = useMetamask();
   const disconnect = useDisconnect();
 
-  const [toggleMenu,setToggleMenu] = useState(false)
-   const [user,setUser] = useState(false)
+  const [toggleMenu, setToggleMenu] = useState(false)
+  const [user, setUser] = useState(false)
 
   const handleLogout = () => {
     setUser(false);
@@ -34,60 +35,60 @@ const Menu = () => (
       <div className="navbar-links">
         <div className="navbar-links_logo">
           <img src={logo} alt="logo" />
-          <Link to="/"> 
+          <Link to="/">
             <h1>CSBS</h1>
           </Link>
         </div>
         <div className="navbar-links_container">
           {/* <input type="text" placeholder='Search Item Here' autoFocus={true} /> */}
-         {address && <Menu />}
-         {/* {user && <Link to="/"><p onClick={handleLogout}>Logout</p></Link> } */}
-        
-        </div> 
+          {address && <Menu />}
+          {/* {user && <Link to="/"><p onClick={handleLogout}>Logout</p></Link> } */}
+
+        </div>
       </div>
       <div className="navbar-sign">
         <>
-          {address ?(
-              <>
-                <button type='button' className='secondary-btn' >{address}</button>
-                <button type='button' className='primary-btn' onClick={disconnect} >Disconnect</button>
-              </>
-            ): (
-              <button type='button' className='secondary-btn' onClick={connectWithMetamask} >Connect MetaMask</button>    
-            )
+          {address ? (
+            <>
+              <button type='button' className='secondary-btn' >{address}</button>
+              <button type='button' className='primary-btn' onClick={disconnect} >Disconnect</button>
+            </>
+          ) : (
+            <button type='button' className='secondary-btn' onClick={connectWithMetamask} >Connect MetaMask</button>
+          )
           }
         </>
       </div>
       <div className="navbar-menu">
-        {toggleMenu ? 
-        <RiCloseLine  color="#fff" size={27} onClick={() => setToggleMenu(false)} /> 
-        : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
+        {toggleMenu ?
+          <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
         {toggleMenu && (
           <div className="navbar-menu_container scale-up-center" >
             <div className="navbar-menu_container-links">
-             <Menu />
+              <Menu />
             </div>
             <div className="navbar-menu_container-links-sign">
-            {user ? (
-              <>
-              <Link to="/create"> 
-                <button type='button' className='primary-btn' >Create</button>
-              </Link>
-              <button type='button' className='secondary-btn'>Connect</button>
-              </>
-            ): (
-              <>
-              <Link to="/login"> 
-              <button type='button' className='primary-btn' onClick={handleLogin} >Sign In</button>
-              </Link>
-              <Link to="/register"> 
-                <button type='button' className='secondary-btn'>Sign Up</button>
-              </Link>
-              </>
-            )}
-           
+              {user ? (
+                <>
+                  <Link to="/create">
+                    <button type='button' className='primary-btn' >Create</button>
+                  </Link>
+                  <button type='button' className='secondary-btn'>Connect</button>
+                </>
+              ) : (
+                <>
+                  <Link to="/login">
+                    <button type='button' className='primary-btn' onClick={handleLogin} >Sign In</button>
+                  </Link>
+                  <Link to="/register">
+                    <button type='button' className='secondary-btn'>Sign Up</button>
+                  </Link>
+                </>
+              )}
+
             </div>
-            </div>
+          </div>
         )}
       </div>
     </div>
---
+++ frontend/src/pages/importSBT/ImportSBT.jsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useState, useCallback } from 'react';
+import './importsbt.css'
+import templayout1 from '../../assets/SBT1-basic.png'
+import templayout2 from '../../assets/SBT2-basic.png'
+import { Modal, Sendsbt } from '../../components'
+const ImportSBT = () => {
+  const [Path, setPath] = useState(templayout1)
+  const [, updateState] = React.useState();
+  const forceUpdate = React.useCallback(() => updateState({}), []);
+  let CommounityData = {
+    CommunityLogo: "",
+    CommunityName: "",
+    CommunityUrl: "",
+  };
+  let SendData = [{
+    id: 1,
+    address: "0x871608cBA092105b91e91295A1d79fFC539BFb48",
+    nickName: "Survivor",
+    roleName: "DeSoc营地分享人"
+  }, {
+    id: 2,
+    address: "0x871608cBA092105b91e91295A1d79fFC539BFb48",
+    nickName: "Rory",
+    roleName: "新人分享者"
+  }];
+
+  useEffect(() => {
+    console.log(Path)
+  }, [Path])
+  const handleLogout = (_s) => {
+    setPath(_s);
+  }
+
+  return (
+    <div className='create section__padding'>
+      <div className="create-container">
+        <div className="flex flex-row justify-between">
+          <h1>Import SBT</h1>
+          <Sendsbt />
+        </div>
+        <div className="flex flex-row">
+          <div className="flex flex-col flex-initial w-4/12">
+            <img className={Path === templayout1 ? "w-64 h-64 border-2 border-solid border-sky-500" : "w-64 h-64"} src={templayout1} onClick={() => { handleLogout(templayout1) }} alt="SBT1" />
+            <img className={Path === templayout2 ? "w-64 h-64 border-2 border-solid border-sky-500" : "w-64 h-64"} src={templayout2} onClick={() => { handleLogout(templayout2) }} alt="SBT2" />
+          </div>
+          <div className='flex-initial w-8/12'>
+            <table className='table w-full'>
+              <thead>
+                <tr>
+                  <th>walletAddress</th>
+                  <th>nickName</th>
+                  <th>roleName</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                <>
+                  {
+                    SendData.map(
+                      (list) =>
+                        <tr className='hover'>
+                          <td>{list.address}</td>
+                          <td>{list.nickName}</td>
+                          <td>{list.roleName}</td>
+                          <td>
+                            <Modal item={list} img={Path} />
+                          </td>
+                        </tr>
+                    )}
+                </>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  )
+};
+
+export default ImportSBT;
