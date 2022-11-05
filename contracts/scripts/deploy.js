4bcc5264533cf1b9774be162d7d6f6fd85bda305
Jhfnetboy(Survivor) <jhfnetboy@gmail.com>
2022-11-05 17:08:33 +0800

    rm hardhat from frontend,add deploy.sh 2 contracts

--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -6,7 +6,7 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 
-async function main() {
+async function main1() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
   const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
   const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
@@ -19,6 +19,15 @@ async function main() {
   await lock.deployed();
 
   console.log("Lock with 1 ETH deployed to:", lock.address);
+}
+
+async function main() {
+  const sbtContractFactory = await hre.ethers.getContractFactory(
+    "CommunitySBT"
+  );
+  const sbtContract = await sbtContractFactory.deploy();
+  await sbtContract.deployed();
+  console.log("Contract deployed to:", sbtContract.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
